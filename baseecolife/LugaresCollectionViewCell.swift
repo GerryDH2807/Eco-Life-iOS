42f7389ed2be96cc5f19bc87a1a1aa7455bcab43
GerryDH2807 <gerardeustua@hotmail.com>
2024-10-31 15:28:21 -0600

    Conexión a la base de datos y funcionamiento logico y visual de la calculadora

---
+++ baseecolife/LugaresCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  LugaresCollectionViewCell.swift
+//  baseecolife
+//
+//  Created by Administrador on 29/10/24.
+//
+
+import Foundation
---
+++ baseecolife/juegoViewController.swift
@@ -0,0 +1,8 @@
+//
+//  juegoViewController.swift
+//  baseecolife
+//
+//  Created by Administrador on 31/10/24.
+//
+
+import Foundation
